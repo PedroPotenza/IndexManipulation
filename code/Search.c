7b953d6fc6040686983ae96e652f7df144629af3
Gabriel Tetzlaf Mansano <tetz.gabriel@gmail.com>
2022-01-13 00:05:30 -0300

    WIP

--- code/Search.c
+++ code/Search.c
@@ -45,54 +45,55 @@ int PrimarySearchV2(KEY fileData)
 int SecondarySearch(char movieName[50])
 {
     
-    // KEY* readKey;
-    // int address;
-    // int nextAddress = 0;
-    // int achou = 0;
+    KEY* readKey;
+    int address;
+    int nextAddress = 0;
+    int achou = 0;
 
-    // int valid = 1;
-    // char MovieNameInteration[50];
+    int valid = 1;
+    char MovieNameInteration[50];
 
-    // for (int i = 0; i < countIndiceSecundario; i++)
-    // {
-    //     if(!strcmp(vetorIndiceSecundario[i].movieName, movieName)) {
-    //         achou = 1;
+    for (int i = 0; i < countIndiceSecundario; i++)
+    {
+        if(!strcmp(vetorIndiceSecundario[i].movieName, movieName)) {
+            achou = 1;
             
-    //         nextOffset = 
-    //     }
-    // }
+            nextOffset = 
+        }
+    }
     
-    // while(fread(&MovieNameInteration, sizeof(char), 50, secondaryIndexFile)) {
+    // ======================================================================= //
+    
+    while(fread(&MovieNameInteration, sizeof(char), 50, secondaryIndexFile)) {
 
-    //     if(!strcmp(MovieNameInteration, movieName)) {
-    //         achou = 1;
-    //         fread(&address, sizeof(int), 1, secondaryIndexFile);
+        if(!strcmp(MovieNameInteration, movieName)) {
+            achou = 1;
+            fread(&address, sizeof(int), 1, secondaryIndexFile);
                         
-    //         nextAddress = address;
+            nextAddress = address;
 
-    //         while(nextAddress != -1) {
-    //             fseek(auxiliarIndexFile, nextAddress, SEEK_SET);
+            while(nextAddress != -1) {
+                fseek(auxiliarIndexFile, nextAddress, SEEK_SET);
 
-    //             fread(&readKey->ClientId, sizeof(int), 1, auxiliarIndexFile);
-    //             fread(&readKey->MovieId, sizeof(int), 1, auxiliarIndexFile);
-    //             fread(&nextAddress, sizeof(int), 1, auxiliarIndexFile);
+                fread(&readKey->ClientId, sizeof(int), 1, auxiliarIndexFile);
+                fread(&readKey->MovieId, sizeof(int), 1, auxiliarIndexFile);
+                fread(&nextAddress, sizeof(int), 1, auxiliarIndexFile);
 
-    //             printf("Codigo do Cliente: %d\n", readKey->ClientId);
-    //             printf("Codigo do Filme: %d\n", readKey->MovieId);
-    //             printf("Nome do Filme: %s\n\n", movieName);
+                printf("Codigo do Cliente: %d\n", readKey->ClientId);
+                printf("Codigo do Filme: %d\n", readKey->MovieId);
+                printf("Nome do Filme: %s\n\n", movieName);
 
-    //         }          
-    //     } else {
-    //         fseek(secondaryIndexFile, sizeof(int), SEEK_CUR);
-    //     }
-    // }
+            }          
+        } else {
+            fseek(secondaryIndexFile, sizeof(int), SEEK_CUR);
+        }
+    }
 
-    // fclose(secondaryIndexFile);
-    // fclose(auxiliarIndexFile);
-    // if (achou == 1) {
-    //     return 1;
-    // } else {
-    //     printf("Nao foi possivel localizar!\n");
-        return 0;
-    // }
+    fclose(secondaryIndexFile);
+    fclose(auxiliarIndexFile);
+    if (achou == 1) {
+        return 1;
+    } else {
+        printf("Nao foi possivel localizar!\n");
+    }
 }
